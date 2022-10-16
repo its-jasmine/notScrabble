bf4bd613b796ecf823d933c49a1b06f941c407e7
Rebecca Elliott <rebeccaelliott35@gmail.com>
2022-10-16 12:06:10 -0400

    init commit of Tile class

---
+++ Tile.java
@@ -0,0 +1,18 @@
+public class Tile {
+
+    private final String letter;
+    private final int value;
+
+    public Tile(String letter, int value) {
+        this.letter = letter;
+        this.value = value;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
