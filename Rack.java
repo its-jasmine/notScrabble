5029f97016caacb6c823842e07e0d704745cb599
ArthurAtangana <contact@arthuratangana.com>
2022-10-19 12:18:02 -0400

    added constant and modified getLetters() in Rack.java

--- Rack.java
+++ Rack.java
@@ -2,28 +2,29 @@ import java.util.*;
 public class Rack {
 
     private List<Letters> lettersList;
-    private int tilesAmount;
+    private int lettersAmount;
+    private final static MAXLETTERS = 7;
 
     public Rack(){
         List lettersList = new ArrayList<Letters>();
-        this.tilesAmount = 0;
+        this.lettersAmount = 0;
     }
     public int getTilesAmount() {
-        return tilesAmount;
+        return lettersAmount;
     }
     public List<Letters> getTilesList() {
         return lettersList;
     }
-    public void getTiles(){
-        if (tilesAmount>7){
+    public void getLetters(){
+        if (lettersAmount>=7){
             System.out.println("The rack already has 7 tiles in it.");
             return;
         }
         //This is for the version that picks up 1 tile from the bag at a time
-        for (int i = 0; i < (7-tilesAmount); i++){
+        for (int i = 0; i < (7-lettersAmount); i++){
 
-            lettersList.add(drawTile());
-            tilesAmount++;
+            lettersList.add(getBag().drawLetter());
+            lettersAmount++;
         }
 
     }
