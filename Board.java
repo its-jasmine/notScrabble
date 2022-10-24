11705f6f9fba0d1b09c51d37af78c91969f96cc7
Victoria Malouf <victoriamalouf@cmail.carleton.ca>
2022-10-24 19:25:50 -0400

    validAlignment java doc

--- Board.java
+++ Board.java
@@ -51,7 +51,7 @@ public class Board {
     /**
      * Determine if the tiles the player is attempting to place are connected to an existing tile.
      * @param tilesPlacedCoordinates the sorted coordinates of the tiles the player is
-     * attempting to place this turn which are confirmed to be in a straight line
+     * attempting to place this turn, which are confirmed to be in a straight line
      * @return true if the sorted tiles are attached to another tile, false otherwise
      */
     private boolean verifyWordAttachment(List<Coordinate> tilesPlacedCoordinates){
@@ -91,7 +91,7 @@ public class Board {
     /**
      * Determine if each square between the first and last tile played is NOT empty.
      * @param tilesPlacedCoordinates the sorted coordinates of the tiles the player is
-     * attempting to place this turn which are confirmed to be in a straight line
+     * attempting to place this turn, which are confirmed to be in a straight line
      * @return true if the sorted tiles placement do no have gaps, false otherwise
      */
     private boolean verifyNoGaps(List<Coordinate> tilesPlacedCoordinates){
@@ -113,7 +113,7 @@ public class Board {
     /**
      * Determine if one of the coordinates attempting to be placed is the start square coordinate.
      * @param tilesPlacedCoordinates the sorted coordinates of the tiles the player is
-     * attempting to place this turn which are confirmed to be in a straight line
+     * attempting to place this turn, which are confirmed to be in a straight line
      * @return true if one of the tilesPlacedCoordinates land on the start square, false otherwise
      */
     private boolean isOnStart(List<Coordinate> tilesPlacedCoordinates){
