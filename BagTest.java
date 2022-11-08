339aab417fb99b082c77dffa0522faaa08c17539
Rebecca-Elliott <rebeccaelliott35@gmail.com>
2022-11-08 15:46:58 -0500

    Bag Unit test

---
+++ BagTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class BagTest {
+    Bag b;
+
+    @Test
+    public void drawTiles() {
+        b = new Bag();
+        assertEquals(7, b.drawTiles(7).size());
+        assertEquals(0, b.drawTiles(0).size());
+        b.drawTiles(91); // bag now empty
+        assertEquals(0, b.drawTiles(19).size());
+    }
+
+    @Test
+    public void returnTiles(){
+        b = new Bag();
+        ArrayList<Tile> drawn = b.drawTiles(1);
+        b.returnTiles(drawn);
+        assertEquals(Bag.MAX_TILES, b.getNumTilesLeft());
+    }
+
+    @Test
+    public void getNumTilesLeft() {
+        b = new Bag();
+        assertEquals(Bag.MAX_TILES, b.getNumTilesLeft());
+        b.drawTiles(7);
+        assertEquals(Bag.MAX_TILES - 7, b.getNumTilesLeft());
+    }
+}
