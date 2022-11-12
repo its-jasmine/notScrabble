089e53c6e77f47d444e86549fc95b946dd974032
Jasmine Gad El Hak <itsjessyg@gmail.com>
2022-11-11 20:54:45 -0500

    init PlayerTest

---
+++ PlayerTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getRack() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void endTurn() {
+    }
+
+    @Test
+    public void getRackScore() {
+    }
+
+    @Test
+    public void addToScore() {
+    }
+
+    @Test
+    public void takeTurn() {
+    }
+}
