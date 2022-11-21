d84dbfefb5ca940763dca721ab577f4a90147d3f
Jasmine Gad El Hak <itsjessyg@gmail.com>
2022-11-21 00:21:09 -0500

    Added testing for exchange tiles in PlayerTest

--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import javax.sound.midi.SysexMessage;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 
 import static org.junit.Assert.*;
@@ -132,7 +133,7 @@ public class PlayerTest {
             player.getRack().drawTiles();
         }
         //System.out.println(player.submit());
-        assertTrue(player.submit()== Game.Status.OVER);
+        assertEquals(player.submit(), Game.Status.OVER);
 
 
     }
@@ -159,5 +160,95 @@ public class PlayerTest {
         player.resetTurn();
 
         assertTrue(player.getRack().getNumTiles() == 7);
+    }
+    @Test
+    public void exchangeOneTile(){
+        Bag bag = new Bag();
+        player = new Player(new Board(),bag);
+        int numTilesInBag = bag.getNumTilesLeft();
+        int playerScore = player.getScore();
+
+        Rack r = player.getRack();
+        ArrayList<Tile> tiles = r.getTilesList();
+        ArrayList<Tile> firstTile = new ArrayList<>();
+        firstTile.add(tiles.get(0));
+        r.removeTiles(firstTile);
+        player.getRack().getExchangeModel().setValueAt(tiles.get(0),0,0);
+
+        assertTrue(player.exchangeTiles());
+
+        ArrayList<Tile> tilesActual = r.getTilesList();
+        assertEquals(7, tilesActual.size()); // should have a full rack
+        assertNotEquals(tiles.get(0), tilesActual.get(0)); // first tile should be different (exchanged)
+        for (int i = 1; i < tilesActual.size(); i++){
+            assertEquals(tiles.get(i), tilesActual.get(i)); // rest of tiles should be the same
+        }
+        // exchange rack should not have the tile anymore
+        assertNull(r.getExchangeModel().getValueAt(0,0));
+        // after an exchange, the number of tiles in the bag should be the same
+        assertEquals(numTilesInBag,bag.getNumTilesLeft());
+
+        assertEquals(playerScore,player.getScore()); // player should not gain any points
+    }
+    @Test
+    public void exchangeAllTiles(){
+        Bag bag = new Bag();
+        player = new Player(new Board(),bag);
+        int numTilesInBag = bag.getNumTilesLeft();
+        int playerScore = player.getScore();
+
+        Rack r = player.getRack();
+        ArrayList<Tile> tiles = r.getTilesList();
+
+        r.removeTiles(tiles);
+
+        for (int i = 0; i < tiles.size(); i++) {
+            player.getRack().getExchangeModel().setValueAt(tiles.get(i), 0, i);
+        }
+
+        assertTrue(player.exchangeTiles());
+
+        ArrayList<Tile> tilesActual = r.getTilesList();
+        assertEquals(7, tilesActual.size()); // should have a full rack
+        for (int i = 0; i < tiles.size(); i++){
+            assertNotEquals(tiles.get(i), tilesActual.get(i)); // all tiles should be different
+            assertNull(r.getExchangeModel().getValueAt(0,i)); // exchange rack should not have the tiles anymore
+        }
+
+        // after an exchange, the number of tiles in the bag should be the same
+        assertEquals(numTilesInBag,bag.getNumTilesLeft());
+
+        assertEquals(playerScore,player.getScore()); // player should not gain any points
+    }
+    @Test
+    public void exchangeUnsuccessful(){
+        Bag bag = new Bag();
+        player = new Player(new Board(),bag);
+        bag.drawTiles(100);
+        int numTilesInBag = bag.getNumTilesLeft();
+        assertEquals(0, numTilesInBag);// bag is empty
+        int playerScore = player.getScore();
+
+        Rack r = player.getRack();
+        ArrayList<Tile> tiles = r.getTilesList();
+        ArrayList<Tile> firstTile = new ArrayList<>();
+        firstTile.add(tiles.get(0));
+        r.removeTiles(firstTile);
+        player.getRack().getExchangeModel().setValueAt(tiles.get(0),0,0);
+
+        assertFalse(player.exchangeTiles());
+
+        ArrayList<Tile> tilesActual = r.getTilesList();
+        assertEquals(6, tilesActual.size()); // should still have one removed tile
+        for (int i = 0; i < tilesActual.size(); i++){
+            assertEquals(tiles.get(i+1), tilesActual.get(i)); // rest of tiles should be the same
+        }
+        // exchange rack should still have the tile
+        assertEquals(firstTile.get(0), r.getExchangeModel().getValueAt(0,0));
+
+        // the number of tiles in the bag should be the same
+        assertEquals(numTilesInBag,bag.getNumTilesLeft());
+
+        assertEquals(playerScore,player.getScore()); // player should not gain any points
     }
 }
