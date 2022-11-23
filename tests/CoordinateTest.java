f91955b7f6019ee739160ce98a9a81e1b9012d4c
Victoria Malouf <victoriamalouf@cmail.carleton.ca>
2022-11-22 22:49:22 -0500

    CoordinateTest refactor

--- tests/CoordinateTest.java
+++ tests/CoordinateTest.java
@@ -27,13 +27,11 @@ public class CoordinateTest {
         }
     }
 
+    /** Tests sorting (A,ONE), (B,ONE), (C, ONE), ... (O, ONE) from reverse */
     @Test
-    public void sortByColumn() {
+    public void sortByColumnFromReverse(){
         Coordinate.Column[] columnValues = Coordinate.Column.values();
         ArrayList<Coordinate> expected = (ArrayList<Coordinate>) Arrays.asList(columnValues).stream().map(col -> new Coordinate(col, Coordinate.Row.ONE)).collect(Collectors.toList());
-
-
-        /*Test 1: Sorting (A,ONE), (B,ONE), (C, ONE), ... (O, ONE) from reverse*/
         List<Coordinate.Column> reversedColumnValues = Arrays.asList(columnValues.clone());
         Collections.reverse(reversedColumnValues);
 
@@ -43,11 +41,17 @@ public class CoordinateTest {
         for (int i = 0; i < coordsToSort.size(); i++){
             assertEquals(expected.get(i), coordsToSort.get(i));
         }
+    }
 
-        /*Test 2: Sorting (A,ONE), (B,ONE), (C, ONE), ... (O, ONE) from shuffle*/
+    /** Tests sorting (A,ONE), (B,ONE), (C, ONE), ... (O, ONE) from shuffle */
+    @Test
+    public void sortByColumnFromShuffle(){
+        Coordinate.Column[] columnValues = Coordinate.Column.values();
+        ArrayList<Coordinate> expected = (ArrayList<Coordinate>) Arrays.asList(columnValues).stream().map(col -> new Coordinate(col, Coordinate.Row.ONE)).collect(Collectors.toList());
         List<Coordinate.Column> shuffledColumnValues = Arrays.asList(columnValues.clone());
         Collections.shuffle(shuffledColumnValues);
-        coordsToSort = (ArrayList<Coordinate>) shuffledColumnValues.stream().map(col -> new Coordinate(col, Coordinate.Row.ONE)).collect(Collectors.toList());
+
+        ArrayList<Coordinate> coordsToSort = (ArrayList<Coordinate>) shuffledColumnValues.stream().map(col -> new Coordinate(col, Coordinate.Row.ONE)).collect(Collectors.toList());
 
         Coordinate.sortByColumn(coordsToSort);
         for (int i = 0; i < coordsToSort.size(); i++){
@@ -80,30 +84,35 @@ public class CoordinateTest {
     }
 
     @Test
-    public void getAdjacentCoordinate() {
-        /* Test 1: Top left corner */
+    public void getAdjacentCoordinateTopLeftCorner(){
         Coordinate c = new Coordinate(Coordinate.Column.A, Coordinate.Row.ONE);
         assertEquals(new Coordinate(Coordinate.Column.B, Coordinate.Row.ONE), c.getAdjacentCoordinate(Coordinate.Adjacent.RIGHT));
         assertEquals(new Coordinate(Coordinate.Column.A, Coordinate.Row.TWO), c.getAdjacentCoordinate(Coordinate.Adjacent.BELOW));
         assertEquals(null, c.getAdjacentCoordinate(Coordinate.Adjacent.LEFT));
         assertEquals(null, c.getAdjacentCoordinate(Coordinate.Adjacent.ABOVE));
+    }
 
-        /* Test 2: Top right corner */
-        c = new Coordinate(Coordinate.Column.O, Coordinate.Row.ONE);
+    @Test
+    public void getAdjacentCoordinateTopRightCorner(){
+        Coordinate c = new Coordinate(Coordinate.Column.O, Coordinate.Row.ONE);
         assertEquals(new Coordinate(Coordinate.Column.N, Coordinate.Row.ONE), c.getAdjacentCoordinate(Coordinate.Adjacent.LEFT));
         assertEquals(new Coordinate(Coordinate.Column.O, Coordinate.Row.TWO), c.getAdjacentCoordinate(Coordinate.Adjacent.BELOW));
         assertEquals(null, c.getAdjacentCoordinate(Coordinate.Adjacent.RIGHT));
         assertEquals(null, c.getAdjacentCoordinate(Coordinate.Adjacent.ABOVE));
+    }
 
-        /* Test 3: Bottom left corner */
-        c = new Coordinate(Coordinate.Column.A, Coordinate.Row.FIFT);
+    @Test
+    public void getAdjacentCoordinateBottomLeftCorner(){
+        Coordinate c = new Coordinate(Coordinate.Column.A, Coordinate.Row.FIFT);
         assertEquals(new Coordinate(Coordinate.Column.B, Coordinate.Row.FIFT), c.getAdjacentCoordinate(Coordinate.Adjacent.RIGHT));
         assertEquals(new Coordinate(Coordinate.Column.A, Coordinate.Row.FOURT), c.getAdjacentCoordinate(Coordinate.Adjacent.ABOVE));
         assertEquals(null, c.getAdjacentCoordinate(Coordinate.Adjacent.LEFT));
         assertEquals(null, c.getAdjacentCoordinate(Coordinate.Adjacent.BELOW));
+    }
 
-        /* Test 4: Bottom right corner */
-        c = new Coordinate(Coordinate.Column.O, Coordinate.Row.FIFT);
+    @Test
+    public void getAdjacentCoordinateBottomRightCorner(){
+        Coordinate c = new Coordinate(Coordinate.Column.O, Coordinate.Row.FIFT);
         assertEquals(new Coordinate(Coordinate.Column.N, Coordinate.Row.FIFT), c.getAdjacentCoordinate(Coordinate.Adjacent.LEFT));
         assertEquals(new Coordinate(Coordinate.Column.O, Coordinate.Row.FOURT), c.getAdjacentCoordinate(Coordinate.Adjacent.ABOVE));
         assertEquals(null, c.getAdjacentCoordinate(Coordinate.Adjacent.RIGHT));
