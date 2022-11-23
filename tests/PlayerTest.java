65acf682ee1fc21c476fa5df76eae51169b92366
ArthurAtangana <arthur.atangana@protonmail.com>
2022-11-22 22:42:11 -0500

    updated PlayerTest.java

--- tests/PlayerTest.java
+++ tests/PlayerTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.sound.midi.SysexMessage;
@@ -11,22 +12,33 @@ public class PlayerTest {
 
     Player player;
     Bag bag;
+    Board board;
     ArrayList<Tile> tiles;
     ArrayList<Coordinate> coordinates;
+    HashSet<Coordinate> coordinateHashSet;
+
+    @Before
+    public void setUp() throws Exception {
+        bag = new Bag();
+        board = new Board();
+        player = new Player(board, bag);
+        tiles = new ArrayList<>();
+        coordinates = new ArrayList<>();
+        coordinateHashSet = new HashSet<>();
+    }
 
     @Test
     public void addToScore() {
-        player = new Player(new Board(),new Bag());
         player.addToScore(0);
-        assertTrue(player.getScore() == 0);
+        assertEquals(player.getScore(), 0);
         player.addToScore(1);
-        assertTrue(player.getScore() == 1);
+        assertEquals(player.getScore(), 1);
         player.addToScore(999);
-        assertTrue(player.getScore() == 1000);
+        assertEquals(player.getScore(), 1000);
         player.addToScore(-1);
-        assertTrue(player.getScore() == 999);
+        assertEquals(player.getScore(), 999);
         player.addToScore(-1000);
-        assertTrue(player.getScore() == -1);
+        assertEquals(player.getScore(), -1);
     }
 
     @Test
@@ -35,71 +47,69 @@ public class PlayerTest {
     }
 
     @Test
-    public void submit() {
-        Board board = new Board();
-        Bag bag = new Bag();
-        player = new Player(board,bag);
-
+    public void submitValidStartWord() {
         //place AND on the starting tile
-        tiles = new ArrayList<>();
-        coordinates = new ArrayList<>();
         tiles.add(LetterTile.A);
-        coordinates.add(new Coordinate(Coordinate.Column.F,Coordinate.Row.EIGHT));
+        coordinates.add(new Coordinate(Coordinate.Column.F, Coordinate.Row.EIGHT));
         tiles.add(LetterTile.N);
-        coordinates.add(new Coordinate(Coordinate.Column.G,Coordinate.Row.EIGHT));
+        coordinates.add(new Coordinate(Coordinate.Column.G, Coordinate.Row.EIGHT));
         tiles.add(LetterTile.D);
-        coordinates.add(new Coordinate(Coordinate.Column.H,Coordinate.Row.EIGHT));
-        HashSet<Coordinate> coordinateHashSet = new HashSet<>();
+        coordinates.add(new Coordinate(Coordinate.Column.H, Coordinate.Row.EIGHT));
         coordinateHashSet.addAll(coordinates);
-        board.placeTiles(coordinates,tiles);
+        board.placeTiles(coordinates, tiles);
         board.setPlayedThisTurn(coordinateHashSet);
 
-        assertTrue(player.submit()== Game.Status.RUNNING);
-
-        // place AND but not connected to another word
-        tiles = new ArrayList<>();
-        coordinates = new ArrayList<>();
+        assertSame(player.submit(), Game.Status.RUNNING);
+    }
+    @Test
+    public void submitUnconnectedValidWord() {
+        //place AND on the starting tile
         tiles.add(LetterTile.A);
-        coordinates.add(new Coordinate(Coordinate.Column.A,Coordinate.Row.EIGHT));
+        coordinateHashSet.add(new Coordinate(Coordinate.Column.F, Coordinate.Row.EIGHT));
         tiles.add(LetterTile.N);
-        coordinates.add(new Coordinate(Coordinate.Column.B,Coordinate.Row.EIGHT));
+        coordinates.add(new Coordinate(Coordinate.Column.G, Coordinate.Row.EIGHT));
         tiles.add(LetterTile.D);
-        coordinates.add(new Coordinate(Coordinate.Column.C,Coordinate.Row.EIGHT));
+        coordinates.add(new Coordinate(Coordinate.Column.H, Coordinate.Row.EIGHT));
+        coordinateHashSet.addAll(coordinates);
+        board.placeTiles(coordinates, tiles);
+        board.setPlayedThisTurn(coordinateHashSet);
+        tiles.clear();
+        coordinates.clear();
+        coordinateHashSet.clear();
+        //add unconnected valid word
+        tiles.add(LetterTile.A);
+        coordinates.add(new Coordinate(Coordinate.Column.A, Coordinate.Row.EIGHT));
+        tiles.add(LetterTile.N);
+        coordinates.add(new Coordinate(Coordinate.Column.B, Coordinate.Row.EIGHT));
+        tiles.add(LetterTile.D);
+        coordinates.add(new Coordinate(Coordinate.Column.C, Coordinate.Row.EIGHT));
         coordinateHashSet = new HashSet<>();
         coordinateHashSet.addAll(coordinates);
-        //player = new Player(board, bag);
-        board.placeTiles(coordinates,tiles);
+        board.placeTiles(coordinates, tiles);
         board.setPlayedThisTurn(coordinateHashSet);
-        //System.out.println(board.getPlayedThisTurn());
 
-        assertTrue(player.submit()== Game.Status.RETRY);
-
+        assertSame(player.submit(), Game.Status.RETRY);
+    }
+    @Test
+    public void submitInvalidWord() {
         // place invalid word
         tiles = new ArrayList<>();
         coordinates = new ArrayList<>();
         tiles.add(LetterTile.L);
-        coordinates.add(new Coordinate(Coordinate.Column.F,Coordinate.Row.NINE));
+        coordinates.add(new Coordinate(Coordinate.Column.F, Coordinate.Row.EIGHT));
         tiles.add(LetterTile.D);
-        coordinates.add(new Coordinate(Coordinate.Column.G,Coordinate.Row.TEN));
+        coordinates.add(new Coordinate(Coordinate.Column.G, Coordinate.Row.EIGHT));
         tiles.add(LetterTile.S);
-        coordinates.add(new Coordinate(Coordinate.Column.H,Coordinate.Row.ELEVEN));
-        coordinateHashSet = new HashSet<>();
+        coordinates.add(new Coordinate(Coordinate.Column.H, Coordinate.Row.EIGHT));
         coordinateHashSet.addAll(coordinates);
-        //player = new Player(board,new Bag());
-        board.placeTiles(coordinates,tiles);
+        board.placeTiles(coordinates, tiles);
         board.setPlayedThisTurn(coordinateHashSet);
-        //System.out.println(board.getPlayedThisTurn());
-        //System.out.println(player.submit());
-        assertTrue(player.submit()== Game.Status.RETRY);
-        board.resetPlayedThisTurn();
 
-        board = new Board();
-        bag = new Bag();
-        player = new Player(board,bag);
-
-        // no more tiles in bag
-        tiles = new ArrayList<>();
-        coordinates = new ArrayList<>();
+        assertSame(player.submit(), Game.Status.RETRY);
+    }
+    @Test
+    public void submitNoMoreTiles(){
+        // place a valid word
         tiles.add(LetterTile.A);
         coordinates.add(new Coordinate(Coordinate.Column.F,Coordinate.Row.EIGHT));
         tiles.add(LetterTile.N);
@@ -110,54 +120,39 @@ public class PlayerTest {
         coordinateHashSet.addAll(coordinates);
         board.placeTiles(coordinates,tiles);
         board.setPlayedThisTurn(coordinateHashSet);
-
-        assertTrue(player.submit()== Game.Status.RUNNING);
-        tiles = new ArrayList<>();
-        coordinates = new ArrayList<>();
+        tiles.clear();
+        coordinates.clear();
+        coordinateHashSet.clear();
+        // place a valid word with no more tiles in bag
         tiles.add(LetterTile.L);
         coordinates.add(new Coordinate(Coordinate.Column.F,Coordinate.Row.SEVEN));
         tiles.add(LetterTile.P);
         coordinates.add(new Coordinate(Coordinate.Column.F,Coordinate.Row.NINE));
         tiles.add(LetterTile.S);
         coordinates.add(new Coordinate(Coordinate.Column.F,Coordinate.Row.TEN));
-        coordinateHashSet = new HashSet<>();
         coordinateHashSet.addAll(coordinates);
-        //player = new Player(board,new Bag());
         board.placeTiles(coordinates,tiles);
         board.setPlayedThisTurn(coordinateHashSet);
-        //System.out.println(board.getPlayedThisTurn());
-        for (int i=0; i<14; i++) {
-            //System.out.println(player.getRack());
+        for (int i=0; i<15; i++) {
             player.getRack().removeTiles(player.getRack().getTilesList());
             player.getRack().drawTiles();
         }
-        //System.out.println(player.submit());
-        assertTrue(player.submit()== Game.Status.OVER);
 
-
+        assertSame(player.submit(), Game.Status.OVER);
     }
 
     @Test
     public void resetTurn() {
-        Board board = new Board();
-        Bag bag = new Bag();
-        player = new Player(board,bag);
-
-        player = new Player(new Board(),new Bag());
-        //place AND on the starting tile
-        tiles = new ArrayList<>();
-        coordinates = new ArrayList<>();
         tiles.add(LetterTile.A);
         coordinates.add(new Coordinate(Coordinate.Column.F,Coordinate.Row.EIGHT));
         tiles.add(LetterTile.N);
         coordinates.add(new Coordinate(Coordinate.Column.G,Coordinate.Row.EIGHT));
         tiles.add(LetterTile.D);
         coordinates.add(new Coordinate(Coordinate.Column.H,Coordinate.Row.EIGHT));
-        HashSet<Coordinate> coordinateHashSet = new HashSet<>();
         coordinateHashSet.addAll(coordinates);
         board.placeTiles(coordinates,tiles);
         player.resetTurn();
 
-        assertTrue(player.getRack().getNumTiles() == 7);
+        assertEquals(player.getRack().getNumTiles(), 7);
     }
 }
